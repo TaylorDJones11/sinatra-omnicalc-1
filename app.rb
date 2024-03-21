2b0b6590a8bdae3a4f686b5f20a11f6f1a5698b6
Taylor D Jones <72883059+TaylorDJones11@users.noreply.github.com>
2024-03-21 01:04:05 +0000

    renaming views

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  erb(:homepageform)
+  erb(:square_new)
 end
 
 get("/:calculation/new") do
