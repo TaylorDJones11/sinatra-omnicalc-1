bfca7520339dfc9a1c4bfb66f5110b8b26fb8189
Taylor D Jones <72883059+TaylorDJones11@users.noreply.github.com>
2024-03-21 01:38:09 +0000

    updating views/results

--- app.rb
+++ app.rb
@@ -10,9 +10,29 @@ get("/") do
   erb(:square_new)
 end
 
-get("/:calculation/new") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/square/new") do
+ erb(:square_new)
 end
+
+get("/square/results") do
+  @sq_number = params.fetch("user_number").to_f
+  @sq_result = @sq_number ** 2
+  erb(:square_results)
+end
+
+get("/square_root/new")do
+ erb(:squareroot_new)
+end
+
+get("/square_root/results") do
+  @sqr_number = params.fetch("number").to_f
+  @sqr_result = Math.sqrt(@sqr_number)
+  erb(:square_root_results)
+end
+
+
+get("/payment/new") do
+  erb(:payment_new)
+end
+
+
